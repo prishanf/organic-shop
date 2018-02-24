f0f1b666f2da5059051eee5f070cb584ca887e78
Prishan Fernando <prishanf@yahoo.com>
2018-02-24 09:58:20 -0800

    implement the product edit ,

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,8 +1,8 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
-
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -12,12 +12,19 @@ import { Component, OnInit } from '@angular/core';
 export class ProductFormComponent implements OnInit {
 
   categories$;
+  product = {};
   
   constructor(
     private router:Router,
+    private route:ActivatedRoute,
     private categoryService: CategoryService, 
     private productService: ProductService) {
     this.categories$ = this.categoryService.getCategories();
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id){
+      this.productService.get(id).take(1).subscribe(p =>this.product = p);
+    }
   }
 
   save(product){
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,13 +5,17 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   constructor(private db: AngularFireDatabase) { }
-
+  //create new product
   create(product){
     return this.db.list('/products').push(product);
   }
-
+  // get all products
   getAll(){
     return this.db.list('/products');
+  }
+  //get product by product Id
+  get(productId){
+    return this.db.object('/products/'+productId);
   }
 
 }
