18b22f8ca2d4606499864de61046bb9737856195
Prishan Fernando <prishanf@yahoo.com>
2018-02-24 10:33:58 -0800

    implement the product filter on the manage product page

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,34 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit,OnDestroy {
 
-  products$;
+  products: { title: string }[];
+  filterdProducts: any[];
+  subscription: Subscription;
   
   constructor(private productService: ProductService) { 
-    this.products$ = productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(prods=>this.filterdProducts = this.products = prods);
+  }
+
+  filter(query: string){
+    this.filterdProducts = (query)? 
+      this.products.filter(p=>p.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())) :
+      this.filterdProducts = this.products; 
   }
 
   ngOnInit() {
+  
+  }
+ 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
