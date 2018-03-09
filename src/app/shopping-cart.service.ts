f58751148076bc674aaca2cc1ff6e7710368993a
Prishan Fernando <prishanf@yahoo.com>
2018-03-09 06:41:14 -0800

    refactor shopping cart service

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -12,13 +12,9 @@ export class ShoppingCartService {
   
   async addToCart(product: Product){
     let cartId = await this.getOrCreateCartId();
-    let item$ = this.db.object('/shopping-carts/' + cartId +'/items/'+ product.$key);
+    let item$ =  this.getItem(cartId,product.$key);
     item$.take(1).subscribe(item=>{
-       if(item.$exists()){
-         item$.update({quantity:item.quantity + 1});
-       }else{
-        item$.set({product:product,quantity :1});
-       } 
+      item$.update({product:product, quantity: (item.quantity || 0) + 1 });
     })
  
   }
@@ -60,5 +56,9 @@ export class ShoppingCartService {
 
   getCart(cartId:String){
     return this.db.object('/shopping-carts/'+this.cartId);
+  }
+
+  private getItem(cartId: String, productId: string){
+    return this.db.object('/shopping-carts/' + cartId +'/items/'+ productId);
   }
 }
